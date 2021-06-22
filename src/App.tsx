3634e02b7dbe7e5f20cb3dd4d00f7f74bfbc7b24
caioharuo <caio.haruo@hotmail.com>
2021-06-22 00:05:42 -0300

    Aula 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
+
 function App() {
-  return <h1>Hello, World</h1>;
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
