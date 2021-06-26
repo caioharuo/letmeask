66b6c4cf1d3c7043ce361e3ec7950f249ff7532a
caioharuo <caio.haruo@hotmail.com>
2021-06-26 19:37:41 -0300

    Adding responsive layout

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,5 +1,7 @@
 import copyImg from "../assets/images/copy.svg";
 
+import { useTheme } from "../hooks/useTheme";
+
 import "../styles/room-code.scss";
 
 type RoomCodeProps = {
@@ -11,8 +13,10 @@ export function RoomCode(props: RoomCodeProps) {
     navigator.clipboard.writeText(props.code);
   }
 
+  const { theme } = useTheme();
+
   return (
-    <button className="room-code" onClick={copyRoomCodeToClipboard}>
+    <button className={`room-code ${theme}`} onClick={copyRoomCodeToClipboard}>
       <div>
         <img src={copyImg} alt="Copiar código da sala" />
       </div>
